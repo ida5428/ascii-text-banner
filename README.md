5560b0efcc02adf7e2e79c0e4925d1f61a44cd9c
ida5428 <207427661+ida5428@users.noreply.github.com>
2025-09-14 00:49:36 +1000

    Added the scrolling text option

--- README.md
+++ README.md
@@ -1,44 +1,55 @@
+<div align="center">
+   <img src="assets/ascii-text-banner.gif" alt="ascii-text-banner preview">
+</div>
+
 # ascii-text-banner
-A simple C++ command-line program to generate ASCII text banners.
+A lightweight C++ command-line tool for generating ASCII text banners.
+
+## Features
+- Render text into ASCII art
+- Optional scrolling effect
+- More features such as outputting files and multiple fonts coming soon!
+- Upcoming features: file output and support for multiple built-in and custom fonts!
+
+## Dependencies
+- A C++17 (or newer) compiler
+- [CMake](https://cmake.org/) >= 3.16
+- [CLI11](https://github.com/CLIUtils/CLI11) (included in `libs/`)
 
 ## Build & Run Instructions
-### Prerequisites
-- [CMake](https://cmake.org/) >= 3.16
-- A C++17 (or later) Compiler
-
-1. Clone this repository:
+### 1. Clone this repository;
 ```bash
 git clone https://github.com/ida5428/ascii-text-banner.git
 ```
 
-2. Navigate into the project directory: 
+### 2. Navigate into the project directory:
 ```bash
 cd ascii-text-banner
 ```
 
-3. Generate the build system:
+### 3. Generate the build system:
 ```bash
 cmake -S . -B build
 ```
 
-4. Build the project:
+### 4. Build the project:
 ```bash
 cmake --build build
 ```
 
-5. Run the program:
-> [!NOTE]
-> The ``--scroll/-s`` and ``--output/-o`` flags have not yet been implemented, they will have no affect.<br>
-> Run with the `-h` or `--help` flag for more details.
-
+### 5. Run the program:
 ```bash
 ./build/main --input "Hello World!" -o
 ```
 ```bash
-./build/main -i "Some veryyyyyyy long text......" --scroll -v 2
+./build/main -i "Some veryyyyyyy long text......" --scroll -v 10
 ```
 
+> [!NOTE]
+> The `-o/--output` flag is not yet implemented and currently has no effect.<br>
+> Run with `-h` or `--help` for more details.
 
 ## License
-This project is licensed under the [GNU General Public License v3.0](https://www.gnu.org/licenses/gpl-3.0.en.html).  
-[CLI11](https://github.com/CLIUtils/CLI11) uses a custom license which can be found [here](https://github.com/ida5428/ascii-text-banner/blob/main/libs/CLI11/LICENSE).
+This project is licensed under the [GNU General Public License v3.0](https://www.gnu.org/licenses/gpl-3.0.en.html), and its file can be found [here](LICENSE).
+[CLI11](https://github.com/CLIUtils/CLI11) is distributed under a [custom license](https://github.com/CLIUtils/CLI11/blob/main/LICENSE), which is also included [here](libs/CLI11/LICENSE).
+
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <cctype>
 #include <sstream>
 #include <array>
+#include <thread>
+#include <chrono>
 
 #include <sys/ioctl.h>
 #include <unistd.h>
@@ -22,34 +24,9 @@ std::vector<std::string> splitString(const std::string& text, char delimiter) {
    return tokens;
 }
 
-int main(int argc, char** argv) {
-   int termWidth;
-   {
-      struct winsize size;
-      ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
-      termWidth = size.ws_col;
-   }
-
-   CLI::App app{"A CLI tool to generate ASCII text banners."};
-
-   std::string inputText;
-   app.add_option("-i,--input", inputText, "The text to convert to ASCII.")->required();
-   int optionWidth = 0;
-   app.add_option("-w,--width", optionWidth, "The maximum width of the ASCII text.")->transform(CLI::Range(15,  termWidth));
-   int optionSpeed = 5;
-   app.add_option("-v,--speed", optionSpeed, "The speed of scrolling.")->transform(CLI::Range(1, 10));
-
-   bool flagOutput = false;
-   app.add_flag("-o,--output", flagOutput, "Output a text file with the ASCII text.");
-   bool flagScroll = false;
-   app.add_flag("-s,--scroll", flagScroll, "Scroll through the ASCII text as an animation.");
-
-   CLI11_PARSE(app, argc, argv);
-
-
+void printNormalBanner(std::string inputText, int maxWidth) {
    std::array<std::string, 7> lines;
    int currentLineWidth = 0;
-   int maxWidth = (optionWidth != 0) ? optionWidth : termWidth;
 
    for (char textChar : inputText) {
       char inputText = std::tolower(textChar);
@@ -57,7 +34,7 @@ int main(int argc, char** argv) {
       std::vector<std::string> asciiCharLines = splitString(asciiChar, '\n');
       int asciiCharWidth = asciiCharLines[0].length();
 
-      if (currentLineWidth + asciiCharWidth + 2 > maxWidth || inputText == '\n') {
+      if (currentLineWidth + asciiCharWidth + 2 > maxWidth) {
          for (std::string line : lines) {
             std::cout << line << '\n';
          }
@@ -74,6 +51,109 @@ int main(int argc, char** argv) {
 
    for (std::string line : lines) {
       std::cout << line << '\n';
+   }
+}
+
+void printScrollingBanner(std::string inputText, int maxWidth, int scrollingSpeed) {
+   std::array<std::string, 7> lines;
+
+   for (int i = 0; i < lines.size(); i++) {
+      lines[i] += "                ";
+   }
+
+   for (char textChar : inputText) {
+      char inputChar = std::tolower(textChar);
+      std::string asciiChar = ascii::character[inputChar];
+      std::vector<std::string> asciiCharLines = splitString(asciiChar, '\n');
+
+      for (int i = 0; i < lines.size(); i++) {
+         lines[i] += asciiCharLines[i];
+      }
+   }
+
+   std::vector<std::vector<std::string>> lineCharacters;
+
+   for (std::string line : lines) {
+      std::vector<std::string> textLine;
+      int index = 0;
+      
+      while (index < line.size()) {
+         if (line.at(index) == ' ') {
+            textLine.push_back(" ");
+            index++;
+         } else {
+            textLine.push_back(line.substr(index, 3));
+            index += 3;
+         }
+      }
+
+      lineCharacters.push_back(textLine);
+   }
+
+   int offset = 0;
+
+   while (true) {
+      // Clear screen, scroll buffer, and reset cursor
+      std::cout << "\033[2J" << "\033[3J" << "\033[H";
+      std::this_thread::sleep_for(std::chrono::milliseconds(200 / scrollingSpeed));
+
+      for (std::vector<std::string> line : lineCharacters) {
+         for (int i = 0; i < maxWidth; i++) {
+            std::cout << line[(i + offset) % line.size()];
+         }
+         std::cout << '\n';
+      }
+
+      offset++;
+   }
+}
+
+int main(int argc, char** argv) {
+   CLI::App app{"A CLI tool to generate ASCII text banners."};
+
+   bool canOutputFile = false;
+   bool canScrollText = false;
+
+   CLI::Option* flagOutput = app.add_flag(
+      "-o,--output", canOutputFile,
+      "Write the ASCII text to an output file."
+   );
+
+   CLI::Option* flagScroll = app.add_flag(
+      "-s,--scroll", canScrollText,
+      "Display the ASCII text as a scrolling animation."
+   );
+
+   std::string inputText;
+   int maxWidth;
+   int scrollingSpeed = 5;
+
+   // Get the maximum terminal width
+   struct winsize size;
+   ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
+   maxWidth = size.ws_col;
+
+   CLI::Option* optionText = app.add_option(
+      "-i,--input", inputText,
+      "The text to convert to ASCII."
+   )->required();
+
+   CLI::Option* optionWidth = app.add_option(
+      "-w,--width", maxWidth,
+      "The maximum width of the ASCII text."
+   )->transform(CLI::Range(15, maxWidth));
+
+   CLI::Option* optionSpeed = app.add_option(
+      "-v,--speed", scrollingSpeed,
+      "The speed of the scrolling animation."
+   )->transform(CLI::Range(1, 10))->needs(flagScroll);
+
+   CLI11_PARSE(app, argc, argv);
+
+   if (canScrollText) {
+      printScrollingBanner(inputText, maxWidth, scrollingSpeed);
+   } else {
+      printNormalBanner(inputText, maxWidth);
    }
 
    return 0;
